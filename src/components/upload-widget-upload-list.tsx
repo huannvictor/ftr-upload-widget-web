c883b36b86cc4c53e0f181561236591bcb19f3f9
Huann <huannvictor@gmail.com>
2026-01-27 13:43:02 -0300

    chore(package): add @radix-ui/react-progress dependency feat(ui): create upload item component with progress indicator style(css): define extra extra small font size variable (xxs)

--- src/components/upload-widget-upload-list.tsx
+++ src/components/upload-widget-upload-list.tsx
@@ -1,3 +1,16 @@
+import { UploadWidgetUploadItem } from "./upload-widget-upload-item";
+
 export function UploadWidgetUploadList() {
-  return <div className="h-48 px-3">upload list</div>;
+  return (
+    <div className="flex flex-col gap-3 px-3">
+      <span className="font-medium text-xs">
+        Uploaded files <span className="text-zinc-400">(2)</span>
+      </span>
+
+      <div className="flex flex-col gap-2">
+        <UploadWidgetUploadItem />
+        <UploadWidgetUploadItem />
+      </div>
+    </div>
+  );
 }
