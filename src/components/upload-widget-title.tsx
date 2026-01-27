1be102047c311d18421c319f9309c48089191992
Huann <huannvictor@gmail.com>
2026-01-27 14:49:54 -0300

    feat(ui): create circular progress bar component; implement global upload status visualization; display empty state message in upload list

--- src/components/upload-widget-title.tsx
+++ src/components/upload-widget-title.tsx
@@ -1,10 +1,23 @@
 import { UploadCloud } from "lucide-react";
 
 export function UploadWidgetTitle() {
+  const isThereAnyPendingUpload = true;
+  const uploadGlobalPercentage = 66;
+
   return (
     <div className="flex items-center gap-1.5 font-medium text-sm">
       <UploadCloud className="size-4 text-zinc-400" strokeWidth={1.5} />
-      <span>Upload files</span>
+
+      {isThereAnyPendingUpload ? (
+        <span className="flex items-baseline gap-1">
+          Upload
+          <span className="to-zinc-400 text-xs tabular-nums">
+            {uploadGlobalPercentage}%
+          </span>
+        </span>
+      ) : (
+        <span>Upload files</span>
+      )}
     </div>
   );
 }
