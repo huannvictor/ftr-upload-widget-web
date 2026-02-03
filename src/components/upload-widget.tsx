a3cb5f01375a9534086b4193bd16967cbbd77825
Huann <huannvictor@gmail.com>
2026-02-03 11:12:50 -0300

    style(css): add border animation keyframes and properties style(ui): reorder utility classes in main component style(ui): apply animated border and progress state to upload widget

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -7,13 +7,15 @@ import { UploadWidgetMinimizedButton } from "./upload-widget-minimized-button";
 import { UploadWidgetUploadList } from "./upload-widget-upload-list";
 
 export function UploadWidget() {
+  const isThereAnyPendingUpload = true;
   const [isWidgetOpen, toggleWidgetOpen] = useCycle(false, true);
 
   return (
-    <Collapsible.Root onOpenChange={() => toggleWidgetOpen()}>
+    <Collapsible.Root asChild onOpenChange={() => toggleWidgetOpen()}>
       <motion.div
         animate={isWidgetOpen ? "open" : "closed"}
-        className="w-90 overflow-hidden rounded-xl bg-zinc-900 shadow-shape"
+        className="w-90 animate-border overflow-hidden rounded-xl border border-transparent bg-zinc-900 data-[state=closed]:data-[progress=false]:shadow-shape data-[state=closed]:rounded-3xl data-[state=open]:shadow-shape data-[state=closed]:data-[progress=true]:[background:linear-gradient(45deg,#09090B,--theme(--color-zinc-900)_50%,#09090B)_padding-box,conic-gradient(from_var(--border-angle),--theme(--color-zinc-700/.48)_80%,--theme(--color-indigo-500)_86%,--theme(--color-indigo-300)_90%,--theme(--color-indigo-500)_94%,--theme(--color-zinc-600/.48))_border-box]"
+        data-progress={isThereAnyPendingUpload}
         variants={{
           closed: {
             width: "max-content",
